768bb86de14874d89d42ba61481aca2459d4b8fa
gartens <41197811+gartens@users.noreply.github.com>
2024-05-06 14:51:36 +0200

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 
-from distutils.core import setup
+from setuptools import setup
 
 version = os.getenv('version', '0.0').split('.')
 if len(version) != 2:
@@ -11,7 +11,7 @@ with open('org/polypheny/prism/version.py', 'w') as f:
     f.write(f'MINOR_VERSION = {version[1]}\n')
 
 setup(name='polyphenyprism',
-      version='0.1',
+      version=version,
       description='Protobuf files for Polypheny',
       packages=['org/polypheny/prism'],
       install_requires=[
