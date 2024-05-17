78996a8d243167f9896afb9d438534f309708c4a
Marco Vogt <marco.vogt@unibas.ch>
2024-05-17 13:58:57 +0200

    Improve python package

--- setup.py
+++ setup.py
@@ -6,11 +6,16 @@ from setuptools import setup
 # Retrieve 'VERSION' environment variable, default to '0.0' if not found.
 version = os.getenv('VERSION', '0.0')
 
+print(f"Building version: {version}")
+
+if version == '0.0':
+    raise ValueError("Version not set or defaulting to '0.0'. Please set the VERSION environment variable.")
+
 # Attempt to split the version number, default to '0' for both if it fails
 try:
     major, minor = version.split('.')
 except ValueError:
-    major, minor = '0', '0'  # Default to '0.0' if the version isn't in a 'major.minor' format
+    raise ValueError(f"Invalid version format: {version}. Expected 'major.minor' format.")
 
 
 with open('org/polypheny/prism/version.py', 'w') as f:
@@ -26,8 +31,16 @@ setup(name='polypheny-prism-api',
       description='Polypheny Prism API files for Python',
       long_description=long_description,
       long_description_content_type='text/markdown',
+      author="The Polypheny Project",
+      author_email="mail@polypheny.org",
+      url="https://polypheny.com/",
+      project_urls={
+        "Documentation": "https://docs.polypheny.com/en/latest/query_interfaces/prism/protocol",
+        "Code": "https://github.com/polypheny/Polypheny-Prism-API"
+      },
+      license="Apache License, Version 2.0",
       packages=['org/polypheny/prism'],
       install_requires=[
           "protobuf==4.24.3",
       ],
-      )
+)
